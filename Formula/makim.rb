863a1c0ed78cd473036e1f385890f1619871d618
sanjay7178 <saisanjay7660@gmail.com>
2025-11-06 17:06:47 +0530

    add few more dependencies
    Signed-off-by: sanjay7178 <saisanjay7660@gmail.com>

--- Formula/makim.rb
+++ Formula/makim.rb
@@ -3,11 +3,16 @@ class Makim < Formula
 
   desc "Tool that helps organize and simplify helper commands using YAML configuration"
   homepage "http://docs.makim.org/"
-  url "https://github.com/makim-org/makim/archive/refs/tags/1.27.0.tar.gz"
-  sha256 "1149ca58edec452f563b28cfe3918e4486ad6b9b2137e64003213cc6cef91050"
+  url "https://github.com/makim-org/makim/releases/download/1.27.0/makim-1.27.0-py3-none-any.whl"
+  sha256 "df7f26d262ec7f4abc668584b055410c47eb737677a8eb1bfb21a3c1b48149d3"
   license "BSD-3-Clause"
 
   depends_on "python@3.12"
+  depends_on "rust" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libffi"
+  depends_on "openssl@3"
+  depends_on "gcc" => :build
 
   def install
     virtualenv_install_with_resources
