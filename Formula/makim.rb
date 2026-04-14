63d03c3824156c88062d8be5a3dc52ef0cc9ce68
Sai Sanjay <saisanjay7660@gmail.com>
2026-04-14 22:37:56 +0530

    Update version and URLs in makim.rb

--- Formula/makim.rb
+++ Formula/makim.rb
@@ -1,26 +1,26 @@
 class Makim < Formula
   desc "A tool that helps organize and simplify helper commands using YAML configuration"
   homepage "https://github.com/osl-incubator/makim"
-  version "1.2.3"
+  version "1.29.3"
   license "BSD-3-Clause"
 
   depends_on "zlib"
   
   on_linux do
     # on_arm do
-    #   url "https://github.com/osl-incubator/makim/releases/download/v1.2.3/makim-darwin-arm64"
+    #   url "https://github.com/osl-incubator/makim/releases/download/1.2.3/makim-linux-arm64"
     #   sha256 "${ARM64_SHA256}"
 
     # end
 
     on_intel do
-      url "https://github.com/makim-org/makim/releases/download/v#{version}/makim-linux-x86-64"
+      url "https://github.com/makim-org/makim/releases/download/#{version}/makim-linux-x86-64"
       sha256 "sha256:3087dd9ad7aa0218b43341d466ea4dcecb0fde3859a7df4e075e3689c4a7e2ab"
     end
   end
 
   def install
-    bin.install Dir["makim-linux-*"].first => "makim"
+    bin.install Dir["makim-*"].first => "makim"
   end
 
   test do
