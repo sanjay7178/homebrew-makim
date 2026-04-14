05b06cb76ac4d9c4981e9a991f68d918d3407f57
Sai Sanjay <saisanjay7660@gmail.com>
2026-04-14 22:18:05 +0530

    Update Makim formula with new version and homepage

--- Formula/makim.rb
+++ Formula/makim.rb
@@ -1,24 +1,28 @@
 class Makim < Formula
-  include Language::Python::Virtualenv
-
-  desc "Tool that helps organize and simplify helper commands using YAML configuration"
-  homepage "http://docs.makim.org/"
-  url "https://github.com/makim-org/makim/archive/refs/tags/1.27.0.tar.gz"
-  sha256 "1149ca58edec452f563b28cfe3918e4486ad6b9b2137e64003213cc6cef91050"
+  desc "A tool that helps organize and simplify helper commands using YAML configuration"
+  homepage "https://github.com/osl-incubator/makim"
+  version "1.2.3"
   license "BSD-3-Clause"
 
-  depends_on "gcc" => :build
-  depends_on "pkgconf" => :build
-  depends_on "rust" => :build
-  depends_on "libffi"
-  depends_on "openssl@3"
-  depends_on "python@3.12"
+  on_macos do
+    # on_arm do
+    #   url "https://github.com/osl-incubator/makim/releases/download/v1.2.3/makim-darwin-arm64"
+    #   sha256 "${ARM64_SHA256}"
+
+    # end
+
+    on_intel do
+      url "https://github.com/makim-org/makim/releases/download/v#{version}/makim-linux-x86-64"
+      sha256 "${AMD64_SHA256}"
+    end
+  end
 
   def install
-    virtualenv_install_with_resources
+    bin.install Dir["makim-linux-*"].first => "makim"
   end
 
   test do
-    system bin/"makim", "--version"
+    output = shell_output("#{bin}/makim --version")
+    assert_match "Version: #{version}", output
   end
 end
